c448b047e98abe475f9bace06f1d6d67ffa32072
Varun Kuntal <varunkuntal@yahoo.in>
2023-10-13 18:22:23 +0000

    #39 finish implementation of terraform provider

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
   token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
@@ -29,3 +29,14 @@ provider "terratowns" {
 #   content_version = var.content_version
 #   assets_path = var.assets_path
 # }
+
+resource "terratowns_home" "home" {
+  name = "Sustainable Symphony in 2023!"
+  description = <<DESCRIPTION
+Embark on a melodic journey through the serene and sustainable landscapes of our future. Sustainable Symphony is an orchestration of eco-friendly practices, innovative digital solutions, and harmonic living. Here, we create a melody that soothes the earth and nourishes our souls, composing a future where technology and ecology coalesce in a gentle, sustainable embrace. Dive into our harmonious guide, explore green technologies, and join us in composing a future that is a Sustainable Symphony.
+DESCRIPTION
+  #domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "d2p4khshwukv10.cloudfront.net"
+  town = "cooker-cove"
+  content_version = 1
+}
