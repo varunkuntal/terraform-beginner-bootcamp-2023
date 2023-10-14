6499cd497cdc63914b3fb9472cfed5430090028a
Varun Kuntal <varunkuntal@yahoo.in>
2023-10-14 07:03:31 +0530

    #44 test our terra house on terratowns test town (#45)

--- variables.tf
+++ variables.tf
@@ -1,8 +1,16 @@
-variable "bucket_name" {
+variable "terratowns_endpoint" {
   type = string
 }
 
-variable "user_uuid" {
+variable "terratowns_access_token" {
+  type = string
+}
+
+variable "teacherseat_user_uuid" {
+  type = string
+}
+
+variable "bucket_name" {
   type = string
 }
 
